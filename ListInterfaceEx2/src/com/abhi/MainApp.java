70ebcdc7a66f2e33010f8fdf46ac68321deacf78
Abhishek <abhicpatil2192000@gmail.com>
2021-07-23 18:29:14 +0530

    List interface Example 2

---
+++ ListInterfaceEx2/src/com/abhi/MainApp.java
@@ -0,0 +1,16 @@
+package com.abhi;
+import java.util.List;
+import java.util.Arrays;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List colors=Arrays.asList("red","Black","Green");
+		System.out.println("Colors = "+colors);
+		
+//		colors.clear();
+//		System.out.println("Colors = "+colors);
+	}
+
+}
